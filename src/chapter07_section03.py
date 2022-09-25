f014dbd8736d7cbf2bbe30553dd271942653758f
pa5411 <85898452+pa5411@users.noreply.github.com>
2022-09-25 17:25:05 +0100

    fix range of FOR Loop when checking closest values for simulations with smaller distances between grid points

--- src/chapter07_section03.py
+++ src/chapter07_section03.py
@@ -24,7 +24,7 @@ NOZZLE_LENGTH = 3
 GAMMA = 1.4 #ratio of specific heats
 TIME_STEPS = 1400
 COURANT_NUMBER = 0.5
-DX = 0.1 #grid spacing
+DX = 0.05 #grid spacing
 PLOT_RESULTS = True #plot graphical data (boolean)
 INT_PRECISION = np.int16
 FLOAT_PRECISION = np.float32
@@ -289,7 +289,7 @@ x_index_ana_num_diff = []
 x_index_ana = []
 
 
-for ii in np.arange(0.0,3.1,0.1):
+for ii in np.arange(0.0,3+DX,DX):
   closest_value = (np.abs(x_ana - ii)).argmin()
 
   x_index_ana_num_diff.append(closest_value)
@@ -301,7 +301,6 @@ for ii in np.arange(0.0,3.1,0.1):
   if math.isclose(ii%0.3, 0, abs_tol=ABS_TOLERANCE):
     x_index_ana.append(closest_value)
 
-#estimate percentage differences
 D_pc_diff = ((D-D_ana[x_index_ana_num_diff])/D) * 100
 M_pc_diff = ((M-M_ana[x_index_ana_num_diff])/M) * 100
 
